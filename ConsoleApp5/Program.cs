a3a2d6b2e99646ffb857976a6a7ca2f6aa65e01a
nasirisara <99367926+nasirisara@users.noreply.github.com>
2022-02-13 21:39:39 +0100

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i<int.MaxValue )
+            { 
+             
+                Console.WriteLine("please insert num1");
+                    string strnum1 = Console.ReadLine();
+
+                    Console.WriteLine("please insert +, -, *,/ as operators!");
+                   string opera = Console.ReadLine();
+
+                    Console.WriteLine("please insert num2");
+                    string strnum2 = Console.ReadLine();
+
+            
+                
+                
+                
+                double num1, num2;
+                num1 = double.Parse(strnum1);
+                num2 = double.Parse(strnum2);
+
+                  
+
+
+
+                    switch (opera)
+                    {
+                        case "+":
+                            Console.WriteLine(num1 + num2);
+                            break;
+
+                        case "-":
+                            Console.WriteLine(num1 - num2);
+                            break;
+
+                        case "/":
+                        { if (num2 == 0)
+                                Console.WriteLine("invalid number");
+                        else
+                            Console.WriteLine(num1 / num2);
+                            break;
+                        }
+                        
+                       
+
+                        case "*":
+                            Console.WriteLine(num1 * num2);
+                            break;
+
+                    }
+                    i++;
+                        }
+
+
+                Console.ReadKey();
+                }
+
+            }
+        }    
+            
+
+    
